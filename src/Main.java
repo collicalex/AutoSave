d9f0078f9a7c89879a3929402b4ba67727ebf873
Collicalex <bargeton.alexandre@gmail.com>
2017-01-03 20:18:04 +0100

    Fix bug with thread restart

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import gui.Gui;
 
 
-//TODO: Prevent standby
-//TODO: Prevent hibernate
-
 //TODO: Move the "_listen" hack from Property to JTextField2
 //TODO: Option (action) to retrieve lost file from local computer into the backup
 //TODO: Add a last backup datetime somewhere?
--- src/gui/StandbyHibernateBlocker.java
+++ src/gui/StandbyHibernateBlocker.java
@@ -11,51 +11,63 @@ import core.PropertiesListener;
 import core.Property;
 import gui.component.PropertiesPanelListener;
 
-public class StandbyHibernateBlocker extends Thread implements PropertiesListener, PropertiesPanelListener {
+public class StandbyHibernateBlocker implements PropertiesListener, PropertiesPanelListener {
 
 	private Robot _robot;
 	private Point _lastPosition;
 	private int _direction;
+	private Thread _thread;
 	
 	public StandbyHibernateBlocker() {
 		try {
 			_robot = new Robot();
+			_thread = createThread();
 		} catch (AWTException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	@Override
-	public void run() {
-		if (_robot != null) {
-			_lastPosition = MouseInfo.getPointerInfo().getLocation();
-			_direction = 1;
-			while(true) {
-				try {
-					Thread.sleep(60000);
-					Point currentPosition = MouseInfo.getPointerInfo().getLocation();
-					if ((currentPosition.x == _lastPosition.x) && (currentPosition.y == _lastPosition.y)) {
-						_robot.mouseMove(currentPosition.x+_direction, currentPosition.y+_direction);
-						_direction *= -1;
-					}
-					_lastPosition = MouseInfo.getPointerInfo().getLocation();
-	            } catch (InterruptedException ex) {
-	            	Thread.currentThread().interrupt();
-	                break;
-	            }
-	        }
+	private Thread createThread() {
+		if (_robot == null) {
+			return null;
 		}
-    }
+		return new Thread() {
+			@Override
+			public void run() {
+				_lastPosition = MouseInfo.getPointerInfo().getLocation();
+				_direction = 1;
+				while(true) {
+					try {
+						Thread.sleep(60000);
+						Point currentPosition = MouseInfo.getPointerInfo().getLocation();
+						if ((currentPosition.x == _lastPosition.x) && (currentPosition.y == _lastPosition.y)) {
+							_robot.mouseMove(currentPosition.x+_direction, currentPosition.y+_direction);
+							_direction *= -1;
+						}
+						_lastPosition = MouseInfo.getPointerInfo().getLocation();
+		            } catch (InterruptedException ex) {
+		            	Thread.currentThread().interrupt();
+		                break;
+		            }
+		        }
+			}
+		};
+	}
+	
+
 	
 	@Override
 	public void ioOperationStart(Properties properties) {
-		this.start();
+		if (_thread != null) {
+			_thread.start();
+		}
 	}
 
 	@Override
 	public void ioOperationEnd(Properties properties) {
-		if (_robot != null) {
-			this.interrupt();
+		if (_thread != null) {
+			_thread.interrupt();
+			_thread = createThread();
 		}
 	}
 	
